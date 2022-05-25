965931aa5126ab73da1a91fd9bed13d3906cef1c
VladimirRutskiy <vovarutskiy@gmail.com>
2022-05-25 17:28:26 +0300

    The lecture about Contains method

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,47 +1,21 @@
 /// <reference types="Cypress" />
 
-it('By ID', () => {
-    cy.visit('http://www.facebook.com/')
-    cy.get('#email')
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
 });
 
-it('By Class', () => {
-    cy.visit('https://docs.cypress.io/guides/references/configuration')
-    cy.get('.main-content-title')
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in')
 });
 
-it('By Tag', () => {
-    cy.visit('https://docs.cypress.io/guides/references/configuration')
-    cy.get('nav')
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase: false});
 });
 
-it('By Tag value', () => {
-    cy.visit('http://www.facebook.com/')
-    cy.get('[name="pass"]')
-});
-
-it('By different Tag', () => {
-    cy.visit('http://www.facebook.com/')
-    cy.get('[data-testid="open-registration-form-button"][role="button"]')
-});
-
-it('By Contains name', () => {
-    cy.visit('https://next.privat24.ua/')
-    cy.get('*[class^="card"]')
-});
-
-it('Using Get with Find and Eq', () => {
-    cy.visit('https://next.privat24.ua/deposit/open')
-    cy.get('tbody').find('td').find('div').find('button').eq(0)
-});
-
-it('Using Get with Find and Eq', () => {
-    cy.viewport(1800, 700)
-    cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
-    cy.get('main').find('div').find('nav').find('ul').find('li').find('a').eq(0)
-});
-it.only('Using Get with Find and Eq', () => {
-    cy.viewport(1800, 700)
-    cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
-    cy.get('main').find('div').find('nav').find('ul').find('li').find('a').eq(0)
+it.only('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('free from cell phones');
 });
