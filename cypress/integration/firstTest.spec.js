6ff104b979e59e91a7609be8020ec1c4dd0dd6ae
VladimirRutskiy <vovarutskiy@gmail.com>
2022-05-26 16:14:44 +0300

    The lesson about actions with elements

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,63 +1,98 @@
 /// <reference types="Cypress" />
 
-it('SHOULD', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('[data-qa-node="amount"]')
-        .type(100)
-        .should('have.value', 100)
-        .and('be.visible')
+//type
+it('type', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('[data-qa-node="phone-number"]')
+        .type(112233344)
 });
 
-it('EXPECT', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('[data-qa-node="amount"]')
-        .type(100).then( input => {
-            expect(input).to.have.value(100)
-        })
-        
+it('focus', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
 });
 
-
-it('EXPECT', () => {
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.get('[data-qa-value="UAH"]')
-        .should('be.checked')
-        
+it('blur', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
+        .blur()
 });
 
-it('check default value for deposit', () => {
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.get('[data-qa-value="UAH"]')
-        .should('be.checked')
-        
+it('clear', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('[data-qa-node="amount"]')
+        .type(999)
+        .wait(2000)
+        .clear()
 });
 
-it('check is visible archive link', () => {
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.contains('Мої депозити')
+it('submit', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('form[method="post"]')
+        .submit()
+});
+
+it('click', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+        .get('[data-qa-value="manual"]')
+        .click()
+});
+
+it('rightclick', () => {
+    cy.visit('https://example.cypress.io/commands/actions')
+        .contains('Right click to edit')
+        .rightclick()
+});
+
+it('doubleclick', () => {
+    cy.visit('https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Element/dblclick_event/_sample_.examples.html')
+        .contains('My Card')
+        .dblclick()
+});
+
+it('check', () => {
+    cy.visit('http://www.facebook.com/reg/?')
+        .get('input[value="2"]')
+        .check()
+});
+
+it('uncheck', () => {
+    cy.visit('https://en.privatbank.ua/')
+        .get('#switch-input')
+        .check({force: true})
+        .wait(2000)
+        .uncheck({force: true})
+});
+
+it('select', () => {
+    cy.visit('https://www.facebook.com/reg/?lang=en')
+        .get('#month')
+        .select('Dec')
+        .get('#day')
+        .select('03')
+        .get('#year')
+        .select('1994')
+});
+
+it('scrollIntoView', () => {
+    cy.visit('https://next.privat24.ua/?lang=en')
+        .get('[data-qa-node="lang"]')
+        .wait(2000)
+        .scrollIntoView()
+});
+
+it('scrollTo', () => {
+    cy.visit('https://next.privat24.ua/?lang=en')
+        .get('[data-qa-node="lang"]')
+        .wait(2000)
+        cy.scrollTo(0, 500)
+});
+
+it.only('trigger', () => {
+    cy.visit('https://next.privat24.ua/?lang=en')
+        .contains('Services')
+        .wait(2000)
         .trigger('mouseover')
-        .get('#archiveDeposits')
-        .should('be.visible')
-        
-});
-
-it('check is correct attr in button', () => {
-    cy.visit('https://next.privat24.ua/open?lang=en')
-    cy.contains('Show cards')
-        .should('have.attr', 'type')
-        .and('match', /button/)
-        
-});
-
-it('check is correct url', () => {
-    cy.visit('https://next.privat24.ua/open?lang=en')
-    cy.url()
-        .should('eq', 'https://next.privat24.ua/open?lang=en')
-        
-});
-
-it.only('check empty field', () => {
-    cy.visit('https://next.privat24.ua/open?lang=en')
-    cy.get('[type="checkbox"]')
-        .should('be.empty')
 });
